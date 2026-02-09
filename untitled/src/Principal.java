646d6fd046de68026590b6cd06bc93e53b4c9dcf
Uziel <uzmanuel.r@gmail.com>
2026-02-09 18:32:15 -0300

    Proyecto base Java – estructura inicial

--- untitled/src/Principal.java
+++ untitled/src/Principal.java
@@ -1,5 +1,6 @@
-public class Principal { public static void main(String[] args) {
-    System.out.println("Hola mundo");
-}
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Conversor de Monedas iniciado");
+    }
 }
 
